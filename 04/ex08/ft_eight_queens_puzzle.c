924ee74760718f221091c4f364858e7271b83c87
Samuel Karani <samuel.karani@berkeley.edu>
2019-07-24 20:37:29 -0700

    solution to n-queens

--- 04/ex08/ft_eight_queens_puzzle.c
+++ 04/ex08/ft_eight_queens_puzzle.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_eight_queens_puzzle_2.c                         :+:      :+:    :+:   */
+/*   ft_eight_queens_puzzle.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: smbaabu <smbaabu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/24 20:03:10 by smbaabu           #+#    #+#             */
-/*   Updated: 2019/07/24 20:36:54 by smbaabu          ###   ########.fr       */
+/*   Created: 2019/07/24 20:03:05 by smbaabu           #+#    #+#             */
+/*   Updated: 2019/07/24 20:36:56 by smbaabu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,14 +36,6 @@ int check_diagonal(int board[N][N], int i, int j)
 	return 1;
 }
 
-void print_result(int board[N])
-{
-	int i = -1;
-	while (++i < N)
-		printf("%d", board[i]);
-	printf("\n");
-}
-
 void print_board(int board[N][N])
 {
 	for (int i = 0; i < N; i++)
@@ -55,11 +47,10 @@ void print_board(int board[N][N])
 	printf("\n");
 }
 
-int helper(int board[N][N], int res[N], int col[N], int i)
+int helper(int board[N][N], int col[N], int i)
 {
 	if (i == N)
 	{
-		print_result(res);
 		// print_board(board);
 		return 1;
 	}
@@ -70,11 +61,9 @@ int helper(int board[N][N], int res[N], int col[N], int i)
 		{
 			board[i][j] = 1;
 			col[j] = 1;
-			res[i] = j + 1;
-			n += helper(board, res, col, i + 1);
+			n += helper(board, col, i + 1);
 			board[i][j] = 0;
 			col[j] = 0;
-			res[i] = 0;
 		}
 	}
 	return n;
@@ -82,12 +71,12 @@ int helper(int board[N][N], int res[N], int col[N], int i)
 
 int	ft_eight_queens_puzzle(void)
 {
-	int board[N][N], i, col[N], res[N];
+	int board[N][N], i, col[N];
 	i = -1;
 	while (++i < N)
 		memset(board[i], 0, sizeof(int) * N);
 	memset(col, 0, sizeof(int) * N);
-	return helper(board, res, col, 0);
+	return helper(board, col, 0);
 }
 
 int main()
